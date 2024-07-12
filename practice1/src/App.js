6ed1f24dc07c450f16a452de27ccb3ea91542d16
balla-sri <srikanthballa4@gmail.com>
2024-07-13 04:22:51 +0530

    props used externally in a component

--- practice1/src/App.js
+++ practice1/src/App.js
@@ -1,29 +1,33 @@
 import './App.css';
+import { User } from './user';
+import { Planet } from './planet';
 
 function App() {
-  const isShowData = true;
+  const users = [
+                {planet: "srikanth", isGasPlanet: 31},
+                {name: "varshika", age: 31},
+                {name: "baby", age: 1},
+  ];
+  const planets = [
+    {planet: "uranus", isGasPlanet: true},
+    {planet: "pluto", isGasPlanet: false},
+    {planet: "neptune", isGasPlanet: true},
+    {planet: "saturn", isGasPlanet: false},
+    {planet: "jupiter", isGasPlanet: true},
+    {planet: "venus", isGasPlanet: true},
+];
+
   return (
-    <div className="App">    
-    {isShowData == true ?  <h1>Showing emp Data</h1> : <h1>No data to show</h1>}
-    {isShowData && <button>This is a button</button>}
-      {/* <Job email="srikanth@gmail.com" salary="24LPA" position="SSA" company="INV"/>
-      <Job email="Karunakar@gmail.com" salary="23LPA" position="ADE" company="BLS"/>
-      <Job email="Shiva@gmail.com" salary="22LPA" position="ACE" company="CAP"/> */}
-      
+    <div className="App">  
+    {users.map((user, key) => {
+    return <User key={key} name={user.name} age={user.age} />
+    }) } 
+    {planets.map((planet, key) => {
+      return <Planet key={key} name={planet.planet} isGas={planet.isGasPlanet} />
+    })}
     </div>
   );
 }
 
-// const Job = (props) => {
-//   return (
-//     <div>
-
-//       <h1>{props.email}</h1>
-//       <h1>{props.position}</h1>
-//       <h1>{props.salary}</h1>
-//       <h1>{props.company}</h1>
-//     </div>
-//   )
-// };
 
 export default App;
