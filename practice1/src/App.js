3e49bcb48ae5923ecdfda9de4a90050e03e0fd37
balla-sri <srikanthballa4@gmail.com>
2024-07-13 01:29:02 +0530

    basic working with components and props

--- practice1/src/App.js
+++ practice1/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Job email="srikanth@gmail.com" salary="24LPA" position="SSA" company="INV"/>
+      <Job email="Karunakar@gmail.com" salary="23LPA" position="ADE" company="BLS"/>
+      <Job email="Shiva@gmail.com" salary="22LPA" position="ACE" company="CAP"/>
     </div>
   );
 }
+
+const Job = (props) => {
+  return (
+    <div>
+      <h1>{props.email}</h1>
+      <h1>{props.position}</h1>
+      <h1>{props.salary}</h1>
+      <h1>{props.company}</h1>
+    </div>
+  )
+};
 
 export default App;
