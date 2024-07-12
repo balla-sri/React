f576285eb5096bcc97283d184f8c98787c87f9aa
balla-sri <srikanthballa4@gmail.com>
2024-07-13 03:25:22 +0530

    Using terinary operator in react

--- practice1/src/App.js
+++ practice1/src/App.js
@@ -1,24 +1,29 @@
 import './App.css';
 
 function App() {
+  const isShowData = true;
   return (
-    <div className="App">
-      <Job email="srikanth@gmail.com" salary="24LPA" position="SSA" company="INV"/>
+    <div className="App">    
+    {isShowData == true ?  <h1>Showing emp Data</h1> : <h1>No data to show</h1>}
+    {isShowData && <button>This is a button</button>}
+      {/* <Job email="srikanth@gmail.com" salary="24LPA" position="SSA" company="INV"/>
       <Job email="Karunakar@gmail.com" salary="23LPA" position="ADE" company="BLS"/>
-      <Job email="Shiva@gmail.com" salary="22LPA" position="ACE" company="CAP"/>
+      <Job email="Shiva@gmail.com" salary="22LPA" position="ACE" company="CAP"/> */}
+      
     </div>
   );
 }
 
-const Job = (props) => {
-  return (
-    <div>
-      <h1>{props.email}</h1>
-      <h1>{props.position}</h1>
-      <h1>{props.salary}</h1>
-      <h1>{props.company}</h1>
-    </div>
-  )
-};
+// const Job = (props) => {
+//   return (
+//     <div>
+
+//       <h1>{props.email}</h1>
+//       <h1>{props.position}</h1>
+//       <h1>{props.salary}</h1>
+//       <h1>{props.company}</h1>
+//     </div>
+//   )
+// };
 
 export default App;
